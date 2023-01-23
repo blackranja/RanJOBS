e3587f9c4ed94fb4754c50137d52523779043ee2
Josphat karanja <josphatranja243@gmail.com>
2023-01-23 20:20:56 +0300

    ExpressJS Router Middleware

---
+++ routes/jobs.js
@@ -0,0 +1,19 @@
+const express = require('expxress');
+const { register } = require('../controllers/auth');
+const router = express.Router();
+
+const {
+    getAllJobs,
+    getJob,
+    createJob,
+    updateJob,
+    deleteJob,
+} = require('../controllers/jobs');
+
+
+router.route('/').post(createJob).get(getAllJobs);
+router.route('/:id').get(getJob).delete(deleteJob).patch(updateJob);
+
+
+module.exports = router;
+
