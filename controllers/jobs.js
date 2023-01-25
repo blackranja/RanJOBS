b6accab488385c24247ce1a8350899f33f4800fc
Josphat karanja <josphatranja243@gmail.com>
2023-01-25 18:33:32 +0300

    ExpressJS Controllers

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -52,6 +52,19 @@ const createJob = async (req, res) => {
 }
 
 const deleteJob = async (req, res) => {
+    const {
+        user: { userId },
+        params: { id: jobId },
+
+    } = req;
+    const job = await Job.findByIdAndRemove({
+        _id: jobId,
+        createdBy: userId,
+    })
+    if (!job) {
+        throw new NotFoundError(`No Job With Id ${jobId}`);
+    }
+    res.status(StatusCodes.OK).send()
 
 }
 
