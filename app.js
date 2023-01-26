55080c7deb682c764239525e0f58cac42f3c769a
Josphat karanja <josphatranja243@gmail.com>
2023-01-26 17:31:44 +0300

    Final Stage

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 require('dotenv').config();
 require('express-async-errors');
+
+//Extra Security Packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
+
 const express = require('express');
 const app = express();
 
@@ -17,7 +25,17 @@ const jobsRouter = require('./routes/jobs');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.use(rateLimiter({
+    windowMs: 15 * 60 * 1000,//15 minutes
+    max: 100,//limit each ip to 100 requests per windowMs
+}));
 app.use(express.json());
+//------------------------
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
+
 
 //routes
 app.use('/api/v1/auth', authRouter);
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,14 +1,26 @@
 const { CustomAPIError } = require('../errors');
 const { StatusCodes } = require('http-status-codes');
 const errorHandlerMiddleware = (err, req, res, next) => {
+
+    let customError = {
+        //set default
+        statusCode: err.statusCode || Status.Coded.INTERNAL_SERVER_ERROR,
+        msg: err.message || 'Something went wrong try again later',
+
+    }
+
     if (err instanceof CustomAPIError) {
         return res.status(err.StatusCode).json({
             msg: err.message
         })
     }
-    return res
-        .status(StatusCodes.INTERNAL_SERVER_ERROR)
-        .send('Something Went Wrong Try Again Later')
 
+    if (err.code && err.code === 11000) {
+        customError.msg = `Duplicate value enter for ${err.keyValue} field, please choose another value`
+        customError.statusCode = 400
+    }
+    //return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send('Something Went Wrong Try Again Later')
+    return res.status(customError.statusCode).json({ msg: customError.msg });
+    
 }
 module.exports = errorHandlerMiddleware;
